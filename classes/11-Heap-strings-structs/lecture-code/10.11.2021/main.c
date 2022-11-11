32837edb3abbc76895edd0cc0c231cb2e44e3ec4
WIN-BGHH2H20TQU\user <Kostuan1989@mail.ru>
2022-11-11 12:31:42 +0500

    Update achievement. Update exam questions.

--- classes/11-Heap-strings-structs/lecture-code/10.11.2021/main.c
+++ classes/11-Heap-strings-structs/lecture-code/10.11.2021/main.c
@@ -11,9 +11,11 @@ void first_variant(int size)
 		mass[2 * size + 4] = 99;
 		free(mass);
 	}
-	// 1. uncomfortably!
-	// 2. (size*size*sizeof(int) + sizeof(void*)) memory using
-	// 3. malloc calls 1 time
+	// 0. simple scheme
+	// 1. effective memory management (create a copy of array / free)
+	// 2. uncomfortably!
+	// 3. (size*size*sizeof(int) + sizeof(void*)) memory using
+	// 4. malloc/free calls 1 time
 }
 
 void second_variant(int size)
@@ -31,9 +33,11 @@ void second_variant(int size)
 			free(mass[i]);
 		free(mass);
 	}
-	// 1. comfortable
-	// 2. (size*size*sizeof(int) + size*sizeof(void*) + sizeof(void*)) memory using
-	// 3. malloc calls (size + 1) times
+	// 0. not simple scheme
+	// 1. very bad memory management
+	// 2. comfortable
+	// 3. (size*size*sizeof(int) + size*sizeof(void*) + sizeof(void*)) memory using
+	// 4. malloc/free calls (size + 1) times
 }
 
 void third_variant(int size)
@@ -48,9 +52,11 @@ void third_variant(int size)
 
 	free(buffer);
 	free(mass);
-	// 1. comfortable
-	// 2. (size*size*sizeof(int) + size*sizeof(void*) + 2*sizeof(void*)) memory using
-	// 3. malloc calls (2) times
+	// 0. a little bit harder scheme
+	// 1. a little bit better memory management
+	// 2. comfortable
+	// 3. (size*size*sizeof(int) + size*sizeof(void*) + 2*sizeof(void*)) memory using
+	// 4. malloc calls (2) times
 }
 
 void fourth_variant(int size)
@@ -58,20 +64,23 @@ void fourth_variant(int size)
 	// decrease malloc calls to 1 time
 	int count = (size * size * sizeof(int) + size * sizeof(void*));
 	int** mass = (int**)malloc(count);
+	if (mass)
+	{
+		for (int i = 0; i < size; i++)
+			mass[i] = (int*)(mass + size) + i * size;                                          // mass[i] = mass+size+i*size WRONG!
+		// &mass[4] == start + 25*8 == start + 200
+		// &mass[4][4] == start + 200 + 16
+		mass[size - 1][size - 1] = 99;
 
-	for (int i = 0; i < size; i++)
-		mass[i] = (int*)(mass + size) + i * size; // mass[i] = mass+size+i*size WRONG!
+		// analyze pointers
+		int* start = (int*)mass;
+		int* end = &(mass[size - 1][size - 1]);
+		// theoretical difference with size==5   end-start == 116 for x86 configuration, real difference 116
+		// theoretical difference with size==5   end-start == 136 for x64 configuration, real difference 216!
+		printf_s("start == %p\nend == %p\ndifference == %d", start, end, (end - start) * sizeof(int));
 
-	mass[size - 1][size - 1] = 99;
-
-	// analyze pointers
-	int* start = (int*)mass;
-	int* end = &(mass[size - 1][size - 1]);
-	// theoretical difference with size==5   end-start == 116 for x86 configuration, real difference 116
-	// theoretical difference with size==5   end-start == 136 for x64 configuration, real difference 216!
-	printf("start == %p\nend == %p\ndifference == %d", start, end, (end - start) * sizeof(int));
-
-	free(mass);
+		free(mass);
+	}
 	// 1. comfortable
 	// 2. (size*size*sizeof(int) + size*sizeof(void*) + sizeof(void*)) memory using
 	// 3. malloc calls (1) times
@@ -81,9 +90,9 @@ void fourth_variant(int size)
 //4 variants for int** works
 int main()
 {
-	first_variant(5);
-	second_variant(5);
-	third_variant(5);
+	//first_variant(5);
+	//second_variant(5);
+	//third_variant(5);
 	fourth_variant(5);
 	return 0;
 }
